c1e74e121a47049412722b3c34d91e7f698e0650
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-05-18 16:48:42 +0700

    Prosedur input ( addNode )

--- 114_singlelinkedlist/114_singlelinkedlist.cpp
+++ 114_singlelinkedlist/114_singlelinkedlist.cpp
@@ -7,6 +7,22 @@ struct Node {
     Node* next;
 };
 
+Node* START = NULL;
+
+void addNode() {
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "Masukkan NIM : ";
+    cin >> nim;
+    cout << "Masukkan Nama : ";
+    cin >> nama;
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
+}
+
+
+
 int main()
 {
     std::cout << "Hello World!\n";
