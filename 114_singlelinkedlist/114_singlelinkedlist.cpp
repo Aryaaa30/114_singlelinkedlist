0cfa3222183b8d7a99040f3d8b7a01128cd72753
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-05-18 17:15:44 +0700

    Prosedur untuk mengeksekusi data atau nilai

--- 114_singlelinkedlist/114_singlelinkedlist.cpp
+++ 114_singlelinkedlist/114_singlelinkedlist.cpp
@@ -57,6 +57,19 @@ bool serachNode(int nim, Node* current, Node* previous) {
         current = current->next;
     }
 
+    if (current == NULL)
+    {
+        return false;
+    }
+    else if (current->noMhs == nim)
+    {
+        retunr true;
+    }
+    else
+    {
+        return false;
+    }
+
 }
 
 
