54675d3ae60133eebc886e8b7ca19e10c28bb103
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-05-18 17:34:12 +0700

    Membuat prosedur traverse yang berfungsi untuk  mengunjungi elemen struktur dan membahasnya

--- 114_singlelinkedlist/114_singlelinkedlist.cpp
+++ 114_singlelinkedlist/114_singlelinkedlist.cpp
@@ -89,6 +89,22 @@ bool listEmpty() {
         return false;
 }
 
+void traverse() {
+    if (listEmpty()) {
+        cout << "List kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+    else {
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            cout << "NIM : " << currentNode->noMhs << ", Nama : " << currentNode->next << endl;
+            currentNode = currentNode->next;
+        }
+    }
+}
+
 
 
 int main()
